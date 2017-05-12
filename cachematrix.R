b68223e615f623747e75370c6804af9fcd6ae620
pabloc <pablo.clar@gmail.com>
2017-05-12 14:40:53 -0300

    second try

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# as in the vector example first function makeCache... creates a list with the arguments 
+# that will be used by the second Cache...
+# argument for first function must be the matrix to be inverted
 
+
+makeCacheMatrix <- function(x = matrix()){
+        mat_inv <- NULL
+        set <- function(y) {
+                x <<- y
+                mat_inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) mat_inv <<- solve
+        getinv <- function() mat_inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheInv will receive as argument the result of makeCache
+# function will first test whether there is already a value calculated and if yes, will get 
+# that value without recalculating
+cacheSolve <- function(x, ...){
+        mat_inv <- x$getinv()
+        if (!is.null(mat_inv)){
+                message("getting matrix cached data")
+                return(mat_inv)
+        }
+        matriz <- x$get()
+        mat_inv <- solve(matriz, ...)
+        x$setinv(mat_inv)
+        return(mat_inv)
 }
